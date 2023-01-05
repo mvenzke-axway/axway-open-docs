5b85b10ede83919f6b9bb3e7e2492b49ca76b77a
Wing Hon Lai <wlai@axway.com>
2023-01-05 10:11:49 +0000

    RDAPI-28896 re-enable the new Jenkins preview (#2522)

--- build.sh
+++ build.sh
@@ -28,13 +28,12 @@ set -e
 
 DEBUG=${DEBUG:-false}
 MODE=dev
-while getopts ":np" opt; do
+
+while getopts ":m:" opt; do
     case ${opt} in
-        n ) MODE=nelify
+        m ) MODE=$OPTARG
              ;;
-        p ) MODE=nelify-preview
-             ;;
-        * ) exit 1
+        * ) echo "[ERROR] Invalid option [${OPTARG}]!!";exit 1
             ;;
     esac
 done
@@ -63,7 +62,7 @@ function fCheckoutSubmodule() {
         exit 1
     fi
     # the npm packages doesn't seem to be needed on the netify build server...this is just for developers
-    if [[ "${MODE}" == "dev" ]];then
+    #if [[ "${MODE}" == "dev" ]];then
         echo "[INFO] Install npm packages required by docsy."
     	if [[ ! -d "node_modules" ]];then
             if [[ -f "package.json" ]];then
@@ -74,7 +73,7 @@ function fCheckoutSubmodule() {
     		    npm install -D --save postcss-cli
             fi
     	fi
-    fi
+    #fi
 }
 
 # fCheckAnchorSyntax:
@@ -86,7 +85,6 @@ function fCheckoutSubmodule() {
 #   - they have fixed the problem with "a" but "b" might not get fixed
 #   - so this function is a failsafe to break the build before it gets that far
 #   - also note that using index.html in the link is needed for external links
-#   - TODO: remember to remove this after zoomin fixes the issues
 function fCheckAnchorSyntax() {
     local pattern="/index.html#"
     local fail_build="false"
@@ -189,19 +187,16 @@ function fRunHugo() {
     cd ${BUILD_DIR}
     mkdir public
     case "${MODE}" in
-        "dev") 
-            hugo server
-            ;;
-        "nelify") 
-            hugo
-            # Moving the "publish" directory to the ROOT of the workspace. Netlify can't publish a
-            # different directory even if the "Publish directory" is changed to specify a different directory.
-            mv -f ${BUILD_DIR}/public ${PROJECT_DIR}
-            ;;
-        "nelify-preview") 
-            hugo -b $DEPLOY_PRIME_URL
-            mv -f ${BUILD_DIR}/public ${PROJECT_DIR}
-            ;;
+      "dev") 
+          hugo server
+          ;;
+      "ci")
+          hugo
+          ;;
+      *)
+          echo "[ERROR] Build MODE [${MODE}] is invalid!!"
+          exit 1
+          ;;
     esac
 }
 
